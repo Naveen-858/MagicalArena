9fed4bfee2be2a06c339276c9bfb7eceb5a2a5e2
Naveen-858 <naveensingavarapu.858@gmail.com>
2024-06-09 19:25:06 +0530

    arean test cases was successfully running

--- test/java/com/magicalarena/ArenaTest.java
+++ test/java/com/magicalarena/ArenaTest.java
@@ -1,9 +1,19 @@
 package com.magicalarena;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
+
 public class ArenaTest {
 
-	
+	@Test
+	public void testFight() {
+		Player playerA = new Player(50, 5, 10);
+		Player playerB = new Player(100, 10, 5);
+
+		Arena arena = new Arena(playerA, playerB);
+		arena.fight();
+
+		assertTrue(!playerA.isAlive() || !playerB.isAlive());
+	}
 }
