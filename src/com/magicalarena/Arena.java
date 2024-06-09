29d2a6a8b68b3d2b6657ce4b6712f60cfd8503fc
Naveen-858 <naveensingavarapu.858@gmail.com>
2024-06-09 19:20:28 +0530

    arean methods was creted

--- src/com/magicalarena/Arena.java
+++ src/com/magicalarena/Arena.java
@@ -11,5 +11,40 @@ public class Arena {
 		this.playerB = playerB;
 		this.dice = new Dice();
 	}
+	public void fight() {
+		Player attacker = playerA.getHealth() < playerB.getHealth() ? playerA : playerB;
+		Player defender = attacker == playerA ? playerB : playerA;
+
+		while (attacker.isAlive() && defender.isAlive()) {
+			takeTurn(attacker, defender);
+			if (!defender.isAlive()) {
+				System.out.println("Player " + (defender == playerA ? "A" : "B") + " is dead.");
+				break;
+			}
+			Player temp = attacker;
+			attacker = defender;
+			defender = temp;
+		}
+
+		System.out.println("Game Over");
+	}
+
+	private void takeTurn(Player attacker, Player defender) {
+		int attackRoll = dice.roll();
+		int defenseRoll = dice.roll();
+		int attackDamage = attacker.getAttack() * attackRoll;
+		int defenseStrength = defender.getStrength() * defenseRoll;
+		int damageToDefender = Math.max(0, attackDamage - defenseStrength);
+
+		defender.reduceHealth(damageToDefender);
+
+		System.out.println("Attacker rolls: " + attackRoll + " (damage: " + attackDamage + ")");
+		System.out.println("Defender rolls: " + defenseRoll + " (defense: " + defenseStrength + ")");
+		System.out.println("Defender takes damage: " + damageToDefender);
+		System.out.println("Defender health: " + defender.getHealth());
+	}
+
+
+
 
 }
