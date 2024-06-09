8211855c2077e2c7c309d409378571319b05b7b3
Naveen-858 <naveensingavarapu.858@gmail.com>
2024-06-09 18:55:20 +0530

    Arena class was created with all attributes

--- src/com/magicalarena/Dice.java
+++ src/com/magicalarena/Dice.java
@@ -1,5 +1,19 @@
 package com.magicalarena;
 
+import java.util.Random;
+
 public class Dice {
+	private static final int SIDES = 6;
+    private Random random;
+    
+    public Dice() {
+        this.random = new Random();
+    }
+
+    public int roll() {
+        return random.nextInt(SIDES) + 1;
+    }
+    
+    
 
 }
